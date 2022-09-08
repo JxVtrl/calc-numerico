a77a61e175537de2c5567ca779ca4d3b0bc71d72
Joao <joaoviniciusvitral@hotmail.com>
2022-09-08 15:33:14 -0300

    overflow

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ChakraProvider } from '@chakra-ui/react'
 import { AppProvider } from './context'
+import { ChakraProvider } from '@chakra-ui/react'
 import App from './App'
 import './styles/global.css'
 
